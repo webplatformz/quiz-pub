c6c2d59f85a96ff192608af08f7c11345e76df29
Lucas Schnüriger <lucas.schnueriger@me.com>
2023-05-09 14:30:16 +0200

    Add quiz save interface

---
+++ src/lib/models/quiz-save.model.ts
@@ -0,0 +1,12 @@
+export interface QuizSave {
+  id: string;
+  name: string;
+  rounds: Round[];
+  date: number;
+  deleted: boolean;
+}
+
+export interface Round {
+  name: string;
+  questions: string[];
+}
