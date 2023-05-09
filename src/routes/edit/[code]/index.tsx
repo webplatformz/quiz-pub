266997149c37be7325b3d17bb4e8b4df0a92d6e2
Lucas Schnüriger <lucas.schnueriger@me.com>
2023-05-09 16:52:17 +0200

    Fix linting errors

--- src/routes/edit/[code]/index.tsx
+++ src/routes/edit/[code]/index.tsx
@@ -1,13 +1,13 @@
-import { component$, useSignal, useTask$ } from '@builder.io/qwik';
-import { useLocation } from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
 
 export default component$(() => {
-    const loc = useLocation();
-    return (
+  const loc = useLocation();
+  return (
 
-        <section class="section bright">
-            <h1>Adapting Quiz: {loc.params.code}!</h1>
-            <h1>create questions...</h1>
-        </section>
-    );
+    <section class="section bright">
+      <h1>Adapting Quiz: {loc.params.code}!</h1>
+      <h1>create questions...</h1>
+    </section>
+  );
 });
