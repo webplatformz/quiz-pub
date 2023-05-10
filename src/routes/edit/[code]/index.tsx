10983157945c72df9bba5df4a10dde349ce95da9
Ben Millo <bmillo@gmail.com>
2023-05-10 11:18:53 +0200

    Merge remote-tracking branch 'origin/main'

--- src/routes/edit/[code]/index.tsx
+++ src/routes/edit/[code]/index.tsx
@@ -1,12 +1,12 @@
-import { component$, useSignal, useStyles$ } from '@builder.io/qwik';
-import { Form, routeAction$, routeLoader$, useLocation, z, zod$ } from '@builder.io/qwik-city';
-import type { QuizState } from '~/lib/models/quiz-state.model';
+import {component$, useStore, useStyles$} from '@builder.io/qwik';
+import {Form, routeAction$, useLocation, z, zod$} from '@builder.io/qwik-city';
+import type {QuizState} from '~/lib/models/quiz-state.model';
 
 import editStyles from '../edit.module.css';
 import styles from '../../styles.css?inline';
 
 export const useSubmitFormAction = routeAction$(
-    (props : QuizState) => {
+    (props: QuizState) => {
         console.log('props', props);
         const newQuiz = JSON.parse(props.quizState);
         console.log('Name deserialized', newQuiz.name);
@@ -41,28 +41,34 @@ export default component$(() => {
     return (
         <section class="section bright">
             <h1>Quiz: {loc.params.code} quiz: {quiz.name}! rounds: {quiz.rounds.length}</h1>
-            <Form action={action}>
-                <input type="text" name="name" value={quiz.name} class={editStyles.input} onInput$={(e: any) => quiz.name = e.target.value}/>
-                <button type="button"
-                    onClick$={() => {
-                        quiz.rounds.push({id: quiz.rounds.length,
-                            text: "Init Roundname - " + quiz.rounds.length})
-                    }}>
-                    Add Round
-                </button>
-                <div class="container container-center">
-                    {(quiz.rounds.length && (
-                        <ul>
-                            {quiz.rounds.map((item, index) => (
-                                <li key={`items-${index}`}>{item.text}</li>
-                            ))}
-                        </ul>
-                    )) || <span>No items found</span>}
-                </div>
-                <input type="hidden" name="quizState" value={JSON.stringify(quiz)} />
-                <button type="submit">save</button>
-
-            </Form>
+            <div class="container container-center">
+                <Form action={action}>
+                    <input type="text" name="name" value={quiz.name} class={editStyles.input}
+                           onInput$={(e: any) => quiz.name = e.target.value}/>
+                    <button type="button"
+                            onClick$={() => {
+                                quiz.rounds.push({
+                                    id: quiz.rounds.length,
+                                    text: "Init Roundname - " + quiz.rounds.length
+                                })
+                            }}>
+                        Add Round
+                    </button>
+                    <p>
+                        {(quiz.rounds.length && (
+                            <ul>
+                                {quiz.rounds.map((item, index) => (
+                                    <li key={`items-${index}`}>{item.text}</li>
+                                ))}
+                            </ul>
+                        )) || <span>No rounds present</span>}
+                    </p>
+                    <input type="hidden" name="quizState" value={JSON.stringify(quiz)}/>
+                    <p>
+                        <button type="submit">save</button>
+                    </p>
+                </Form>
+            </div>
 
         </section>
     );
