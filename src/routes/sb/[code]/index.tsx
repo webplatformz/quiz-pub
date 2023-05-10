9b8e3bc0171a7a54dd572fbd4e1d60e0f38a7640
Lucas Schnüriger <lucas.schnueriger@me.com>
2023-05-10 15:34:42 +0200

    Read games and get updates on changes from Supabase

---
+++ src/routes/sb/[code]/index.tsx
@@ -0,0 +1,32 @@
+import { component$ } from "@builder.io/qwik";
+import { routeLoader$, useLocation } from "@builder.io/qwik-city";
+import { createClient } from "@supabase/supabase-js";
+import { createServerClient } from "supabase-auth-helpers-qwik";
+
+const useDBTest = routeLoader$(async (requestEv) => {
+  const supabaseClient = createServerClient(
+    requestEv.env.get("PUBLIC_SUPABASE_URL")!,
+    requestEv.env.get("PUBLIC_SUPABASE_ANON_KEY")!,
+    requestEv
+  );
+  const { data } = await supabaseClient.from('game').select('*')
+  return { data };
+});
+
+export default component$(() => {
+  const supabaseUrl = process.env.PUBLIC_SUPABASE_URL!;
+  const supabaseAnonKey = process.env.PUBLIC_SUPABASE_ANON_KEY!;
+  const supabase = createClient(supabaseUrl, supabaseAnonKey);
+  supabase
+    .channel("changes")
+    .on("postgres_changes", { event: "*", schema: "public", table: "game" }, payload => console.log(payload))
+    .subscribe();
+  const games = useDBTest();
+  const code = useLocation().params.code;
+  return (
+    <>
+      <h1>ROOM: {code}</h1>
+      {games.value}
+    </>
+  );
+});
