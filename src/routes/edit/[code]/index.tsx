e7eb3dee77f74b7bb444fa9714993dcf8a2e8e59
Ben Millo <bmillo@gmail.com>
2023-05-10 11:11:10 +0200

    Adapting Form with state and round list

--- src/routes/edit/[code]/index.tsx
+++ src/routes/edit/[code]/index.tsx
@@ -1,43 +1,65 @@
-import { component$, useSignal } from '@builder.io/qwik';
-import { Form, routeAction$, routeLoader$, useLocation, z, zod$ } from '@builder.io/qwik-city';
-import type { QuizState } from '~/lib/models/quiz-state.model';
+import {component$, useStore} from '@builder.io/qwik';
+import {Form, routeAction$, useLocation, z, zod$} from '@builder.io/qwik-city';
+import type {QuizState} from '~/lib/models/quiz-state.model';
 
 import styles from '../edit.module.css';
 
-const quiz = {} as QuizState;
-
-export const useUpdateNameAction = routeAction$(
-    (props) => {
-        quiz.name = props.name;
+export const useSubmitFormAction = routeAction$(
+    (props : QuizState) => {
         console.log('props', props);
+        const newQuiz = JSON.parse(props.quizState);
+        console.log('Name deserialized', newQuiz.name);
+        //console.log('props', props.quizState.name);
         return {
             success: true,
         };
     },
     zod$({
-        name: z.string().trim(),
+        quizState: z.any()
     })
 );
 
-export const useQuizLoader = routeLoader$(() => {
-    return quiz;
+export const quizInitializier = (() => {
+    return {
+        name: "",
+        joinCode: "",
+        adminCode: "",
+        players: [],
+        rounds: [],
+        questions: [],
+        answers: [],
+        currentRound: 1
+    } as QuizState;
 });
 
 export default component$(() => {
     const loc = useLocation();
-    const quiz = useQuizLoader();
-    const action = useUpdateNameAction();
-    const name = useSignal('');
+    const action = useSubmitFormAction();
+    const quiz = useStore(quizInitializier());
     return (
-
         <section class="section bright">
-            <h1>Adapting Quiz: {loc.params.code} signal: {name.value} quiz: {quiz.value.name}!</h1>
+            <h1>Quiz: {loc.params.code} quiz: {quiz.name}! rounds: {quiz.rounds.length}</h1>
             <Form action={action}>
-
-                <input type="text" name="name" value={quiz.value.name} class={styles.input} onInput$={(e: any) => {
-                    name.value = e.target.value;
-                }} />
+                <input type="text" name="name" value={quiz.name} class={styles.input} onInput$={(e: any) => quiz.name = e.target.value}/>
+                <button type="button"
+                    onClick$={() => {
+                        quiz.rounds.push({id: quiz.rounds.length,
+                            text: "Init Roundname - " + quiz.rounds.length})
+                    }}>
+                    Add Round
+                </button>
+                <div class="container container-center">
+                    {(quiz.rounds.length && (
+                        <ul>
+                            {quiz.rounds.map((item, index) => (
+                                <li key={`items-${index}`}>{item.text}</li>
+                            ))}
+                        </ul>
+                    )) || <span>No items found</span>}
+                </div>
+                <input type="hidden" name="quizState" value={JSON.stringify(quiz)} />
                 <button type="submit">save</button>
+
             </Form>
 
         </section>
