25b63c357b182b2dd484efa85bbc27b49bd8f7e1
Mika Ruch <mika.ruch@zuehlke.com>
2023-05-10 17:39:38 +0200

    fix cloudflare

--- src/entry.cloudflare-pages.tsx
+++ src/entry.cloudflare-pages.tsx
@@ -28,33 +28,29 @@ const fetch = async (request: Request, env: Env, ctx: PlatformCloudflarePages["c
   const url = new URL(request.url);
   const path = url.pathname.slice(1).split("/");
   const isWebsocketRequest = request.headers.get("Upgrade") === "websocket";
-  if (!path[0] && !isWebsocketRequest) {
-    // Serve our HTML at the root path.
-    return createQwikCity({ render, qwikCityPlan, manifest })(request, env, ctx);
-  }
-
+  
   if (isWebsocketRequest) {
     return env.QUIZ_PUB.fetch(request, env, ctx);
   }
 
-  if (path[0] !== "quiz") {
-    return new Response("Not found", { status: 404 });
+  if (path[0] === "quiz") {
+    if (request.method === "PUT") {
+      const uuid = randomId();
+      const value = await env.QUIZ_PUB_KV.get(uuid);
+      if (value) {
+        return new Response("uuid is already taken", { status: 500 });
+      }
+      const quiz = await request.json();
+      await env.QUIZ_PUB_KV.put(uuid, JSON.stringify(quiz));
+      return new Response(uuid);
+    } else if (request.method === "GET") {
+      const id = url.searchParams.get("id");
+      const quiz = await env.QUIZ_PUB_KV.get(id);
+      return new Response(quiz);
+    }
   }
 
-  if (request.method === "PUT") {
-    const uuid = randomId();
-    const value = await env.QUIZ_PUB_KV.get(uuid);
-    if (value) {
-      return new Response("uuid is already taken", { status: 500 });
-    }
-    const quiz = await request.json();
-    await env.QUIZ_PUB_KV.put(uuid, JSON.stringify(quiz));
-    return new Response(uuid);
-  } else if (request.method === "GET") {
-    const id = url.searchParams.get("id");
-    const quiz = await env.QUIZ_PUB_KV.get(id);
-    return new Response(quiz);
-  }
+  return createQwikCity({ render, qwikCityPlan, manifest })(request, env, ctx);
 };
 
 export { fetch };
