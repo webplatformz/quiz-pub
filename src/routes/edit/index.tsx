9cfcfb1b7eb61ef2d3ac34994d87a72be01b0375
Lucas Schnüriger <lucas.schnueriger@me.com>
2023-05-11 11:22:20 +0200

    Load quizzes from localStore ides

--- src/routes/edit/index.tsx
+++ src/routes/edit/index.tsx
@@ -1,22 +1,25 @@
-import { component$, useStyles$ } from "@builder.io/qwik";
+import { component$, useStore, useStyles$, useVisibleTask$ } from "@builder.io/qwik";
 
 import styles from "./styles.css?inline";
-
-export const createShortGUID = () => {
-    const firstPart = (Math.random() * 46656) | 0;
-    return ("0000" + firstPart.toString(36)).slice(-4);
-};
+import { supabase } from "~/lib/db";
+import { quizzesLSKey } from "~/lib/constants";
 
 export default component$(() => {
-    useStyles$(styles);
-    const linkTemplate = `/edit/${createShortGUID()}`;
+  useStyles$(styles);
+  const store = useStore<{ quizzes: any[] }>({ quizzes: [] });
+  useVisibleTask$(async () => {
+    const quizIds = JSON.parse(localStorage.getItem(quizzesLSKey) ?? "[]");
+    store.quizzes = (await supabase
+      .from("quiz")
+      .select()
+      .in("id", quizIds))
+      .data ?? [];
+  });
 
-    return (
-        <section class="section bright container">
-            <h3>Quizzes</h3>
-            <div class="container container-center">
-                <a href={linkTemplate} class="button">Create Quiz</a>
-            </div>
-        </section>
-    );
+  return (
+    <section class="section bright container">
+      <h3>Quizzes</h3>
+      {store.quizzes.map(q => (<h2>{q.name}</h2>))}
+    </section>
+  );
 });
