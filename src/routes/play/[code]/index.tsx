5797ecf5fd5cc632b2700941d9570d1da854fb8d
Mika Ruch <mika.ruch@zuehlke.com>
2023-05-12 11:54:21 +0200

    try to fix ws stuff

--- src/routes/play/[code]/index.tsx
+++ src/routes/play/[code]/index.tsx
@@ -1,5 +1,5 @@
 import type { NoSerialize } from "@builder.io/qwik";
-import { $, component$, noSerialize, useStore, useVisibleTask$ } from "@builder.io/qwik";
+import { component$, useStore, useVisibleTask$ } from "@builder.io/qwik";
 import { useLocation } from "@builder.io/qwik-city";
 import type { StoredQuiz } from "~/routes/all-games";
 
@@ -12,40 +12,32 @@ export default component$(() => {
     const location = useLocation();
     const store = useStore<{ ws: NoSerialize<WebSocket | undefined> }>({ ws: undefined });
     const quiz = useStore<{ players: string[] }>({ players: [] });
-    const join = $(() => {
-        console.log("join");
-        if (store.ws && store.ws.readyState !== WebSocket.CLOSED) {
-            return;
-        }
-        console.log("join 2");
-        const code = location.params.code;
-        const name = new URL(location.url).searchParams.get("name");
-        if (!name) {
-            return;
-        }
-        const quizzes: StoredQuiz[] = JSON.parse(localStorage.getItem("quizzes") ?? "[]");
-        const quiz = quizzes.find(quiz => quiz.id === code);
 
-        try {
-            const auth = quiz ? `&auth=${quiz.adminToken}` : "";
-            const ws = new WebSocket(`wss://${window.location.host}/joinquiz/${code}?name=${name}${auth}`);
-            ws.onopen = () => {
-                store.ws = noSerialize(ws);
-            };
-        } catch (e) {
-            console.log(e);
-        }
-    });
+    useVisibleTask$(async ({ cleanup }) => {
+        const join = (): WebSocket | undefined => {
+            console.log("join");
+            if (store.ws && store.ws.readyState !== WebSocket.CLOSED) {
+                return;
+            }
+            console.log("join 2");
+            const code = location.params.code;
+            const name = new URL(location.url).searchParams.get("name");
+            if (!name) {
+                return;
+            }
+            const quizzes: StoredQuiz[] = JSON.parse(localStorage.getItem("quizzes") ?? "[]");
+            const quiz = quizzes.find(quiz => quiz.id === code);
 
-    useVisibleTask$(async () => {
-        await join();
-    });
+            try {
+                const auth = quiz ? `&auth=${quiz.adminToken}` : "";
+                return new WebSocket(`wss://${window.location.host}/joinquiz/${code}?name=${name}${auth}`);
+            } catch (e) {
+                console.log(e);
+            }
+        };
 
-    useVisibleTask$(({ track, cleanup }) => {
-        track(() => store.ws);
-        if (store.ws && store.ws.readyState === WebSocket.CLOSED) {
-
-            store.ws.onmessage = (msg) => {
+        const addHandlers = (ws: WebSocket) => {
+            ws.onmessage = (msg) => {
                 try {
                     const message: Message = JSON.parse(msg.data);
                     switch (message.type) {
@@ -57,21 +49,33 @@ export default component$(() => {
                     console.log(e);
                 }
             };
-            store.ws.onclose = async () => {
-                console.log("close");
-                await join();
-            };
-            store.ws.onerror = (error) => {
+            ws.onerror = (error) => {
                 console.log(error);
             };
+            ws.onclose = async () => {
+                console.log("close");
+                const newWs = await join();
+                if (!newWs) {
+                    console.log("fuck ws couldnt be opened");
+                    return;
+                }
+                addHandlers(newWs);
+            };
+        };
+
+        const ws = join();
+        if (!ws) {
+            console.log("fuck ws couldnt be openend");
+            return;
         }
+        addHandlers(ws);
         cleanup(() => {
-            console.log("cleanup");
-            store.ws?.close();
+            ws.close();
         });
     });
+
     return <div>
-        <span>Connected: {`${!!store.ws}`}</span>
+        <span>Connected: {`${store.ws?.readyState}`}</span>
         <ul>{
             quiz.players.map((player, index) => {
                 return <li key={index}>{player}</li>;
